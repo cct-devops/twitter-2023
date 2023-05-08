52ce02be637e9293d22a51c66ba7a5a82aa7713f
David Gonzalez <dagonza1983@gmail.com>
2023-05-08 21:01:48 +0100

    stubs for the CA

--- mp3-play/main.py
+++ mp3-play/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
@@ -19,5 +19,23 @@ def serve_ui():
         return Response(f.read(), mimetype="text/html")
     
 
+# POST
+@app.route('<playlist_name>', methods = ["POST"])
+def create_playlist(playlist_name):
+    pass # insert a row in the playlists table.
+
+# PUT
+@app.route('<playlist_name>', methods=["PUT"])
+def add_song_to_playlist(playlist_name):
+    song = request.args.get('song')
+    print(f"adding song {song} into the playlist {playlist_name}")
+    pass
+
+@app.route('<playlist_name>', methods = ["GET"])
+def get_playlist(playlist_name):
+    # go into the database and SELECT all the songs for a given playlist. 
+    # SELECT * FROM songs s, playlists p, songs_in_playlist sip WHERE s.song_id = sip.song_id AND p.playlist_id = sip.playlist_id AND p.name = ?
+    # song (song_id, song_name, song_location)
+    return playlist_name
 if __name__ == '__main__':
     app.run()
